5d91779d10cd714b4cc5a5462f92afb07a4ee5cd
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-10-02 16:51:34 +0700

    list view

--- app/src/main/java/com/example/fundamentalandroidheroes/MainActivity.kt
+++ app/src/main/java/com/example/fundamentalandroidheroes/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.fundamentalandroidheroes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val heroes = arrayOf(
+        "Cut Nyak Dien",
+        "Ki Hajar Dewantara",
+        "Moh Yamin",
+        "Patimura",
+        "R A Kartini",
+        "Sukarno"
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter =
+            ArrayAdapter(
+                this,
+                R.layout.list_item,
+                R.id.textViewHeroName,
+                heroes
+            )
+
+        listViewHeroes.adapter = adapter
     }
 }
